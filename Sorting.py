43b0d1f9c358b557bf3e0b617789a0741fe44159
Sanket Patel <64171455+Sanket14960@users.noreply.github.com>
2022-12-05 23:05:33 -0500

    Update Sorting.py
    Added mergeSort function.

--- Sorting.py
+++ Sorting.py
@@ -45,3 +45,38 @@ def insertionSort(array):
 				array[currentValue], array[k] = array[k], array[currentValue]
 				
 	return array
+
+#Merge Sort Implementation 
+import math
+	
+def mergeSort(array):
+	if len(array) == 1:
+		return array
+
+	# Split Array into right and left 
+	length = len(array)
+	middle = math.floor(length/2)
+	left = array[0:middle]
+	right = array[middle:]
+
+	return merge(
+		mergeSort(left),
+		mergeSort(right)
+	)
+
+def merge(left, right):
+	result = []
+	leftIndex = 0
+	rightIndex = 0
+
+	while leftIndex < len(left) and rightIndex < len(right):
+		if left[leftIndex] < right[rightIndex]:
+			result.append(left[leftIndex])
+			leftIndex += 1
+		else:
+			result.append(right[rightIndex])
+			rightIndex += 1
+
+	return result + left[leftIndex:] + right[rightIndex:]
+
+print(mergeSort(numbers))
